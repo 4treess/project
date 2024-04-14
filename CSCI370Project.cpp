3c2a8a12054d60d14a395349691b058bdb06e1bb
4treess <trevordeclark04@gmail.com>
2024-04-14 10:39:50 -0700

    saving progress

--- CSCI370Project.cpp
+++ CSCI370Project.cpp
@@ -1,37 +1,14 @@
 /*
 Author: Trevor De Clark, Sno: 665182861, CSCI 370 VIU
-Notes: Channel names cannot have spaces. All years must have 3 parameters
-
-Unrelated: The password for logging into the database via C++ is the Yearline interface password
 */
 
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <occi.h>
-#include <termios.h>
-#include <unistd.h>
-#include <string>
-#include <stdexcept>
-using namespace oracle::occi;
+#include "Main.h"
 
 
-// creates a connection to the oracle db by getting the users login info
-Connection* login(Environment* ptr);
-
-// termniates the provided connection and environment
-void logoff(Environment* ptr, Connection* ptr2);
-
-//Gets input from the user (all Years excluding quit have 2 parameters)
-int getUserInput();
 
 //Takes the values out of the Resultset and puts them into the array
 void prepareResults(ResultSet* resSet, int* array);
 void prepareResults(ResultSet* resSet, std::string* names, int* counts);
-
-//Outputs the data from other functions in a file in the format of a report.
-void generateReport(int year, int* newUsers, int* postedMessages, std::ofstream* file);
-void generateReport(std::string* names, int* counts, std::ofstream* file);
 
 
 int main(){
@@ -42,74 +19,20 @@ int main(){
         return 0;
     }
 
-    std::ofstream outputFile;
-    outputFile.open("Report.txt", std::ofstream::trunc);
+    std::string commands[NUM_OF_COMMANDS + 1] = {"search", "add", "delete", "cancel", "complete", "remove", "quit", "\0"};
+    std::string functions[NUM_OF_COMMANDS] = {search(), addEmployee(), deleteEmployee(), complete(), cancel(), assign(), remove(), returnTrue()};
 
-    std::string str1 = "select count(*), to_number(to_char(signUpDate, 'mm')) from Users where to_number(to_char(signUpDate, 'yyyy')) = :1 group by to_number(to_char(signUpDate, 'mm')) order by to_number(to_char(signUpDate, 'mm')) asc";
-    std::string str2 = "select count(*), to_number(to_char(postTime, 'mm')) from messages where to_number(to_char(postTime, 'yyyy')) = :1 group by to_number(to_char(postTime, 'mm')) order by to_number(to_char(postTime, 'mm')) asc";
-    std::string str3 = "select username, nickname, count(messageID) as messageCount from Users left join Messages on Users.username = messages.poster where to_number(to_char(messages.postTime, 'yyyy')) = :1 group by username, nickname order by messageCount desc";
-    std::string str4 = "select channelName, creator, count(messageID) as messageCount from Users left join Channels on Users.username = Channels.creator join messages on channels.channelName = messages.channel where to_number(to_char(messages.postTime, 'yyyy')) = :1 group by channelName, creator order by messageCount desc";
 
-    int year;
-    int newUserCount[12] = {};
-    int messageCount[12] = {};
-
-    std::cout << "Please enter the year you want reports generated for: ";
-    year = getUserInput();
+    std::string input = getUserInput();
+    if(input == "quit"){
+        logoff(enviro, connect);
+        return 0;
+    }
     if(year == -1){
         logoff(enviro, connect);
         return 0;
     }
         try{
-            Statement* stmt = connect->createStatement(str1);
-            stmt->setInt(1, year);
-            ResultSet* resSet1 = stmt->executeQuery();
-            prepareResults(resSet1, newUserCount);
-            stmt->closeResultSet(resSet1);
-
-            stmt = connect->createStatement(str2);
-            stmt->setInt(1, year);
-            ResultSet* resSet2 = stmt->executeQuery();
-            prepareResults(resSet2, messageCount);
-            stmt->closeResultSet(resSet2);
-
-            generateReport(year, newUserCount, messageCount, &outputFile);
-
-            std::string names[6] = {"null", "null", "null", "null", "null", "null"};
-            int counts[3] = {};
-
-            outputFile << "\nSummary report for the year of " << year << "\n\n"
-                       << "Top Three Most Active Users:\n"
-                       << "username         nickname        # of posts\n"
-                       << "--------         -----------     ----------\n";
-
-            stmt = connect->createStatement(str3);
-            stmt->setInt(1, year);
-            ResultSet* resSet3 = stmt->executeQuery();
-            prepareResults(resSet3, names, counts);
-            stmt->closeResultSet(resSet3);
-            generateReport(names, counts, &outputFile);
-
-            outputFile << "\nTop Three Most Active Channels:\n"
-                       << "channelName      creator         # of posts\n"
-                       << "-----------      --------        ----------\n";
-
-            for(int i = 0; i < 6; i++){
-                names[i] = "null";
-            }
-            for(int i = 0; i < 3; i++){
-                counts[i] = 0;
-            }
-
-            stmt = connect->createStatement(str4);
-            stmt->setInt(1, year);
-            ResultSet* resSet4 = stmt->executeQuery();
-            prepareResults(resSet4, names, counts);
-            stmt->closeResultSet(resSet4);
-            generateReport(names, counts, &outputFile);
-            connect->terminateStatement(stmt);
-
-            outputFile << "\n" << std::right << std::setfill('=') << std::setw(32) << "END OF REPORT" << std::setw(18) << " ";
 
         }catch(SQLException* excep){
                 std::cerr << "Error! " << excep << "\n";
@@ -162,112 +85,106 @@ void logoff(Environment* enviro, Connection* connect){
 }
 
 /*
-@brief: returns the user input and confirms its an integer
+@brief: Gets the input from the user, makes all characters lowercase. Uses std::cin so it is whitespace sensitive
 @param: N/A
-@return: int. The year that the user inputted is returned
+@return: std::string, 
 */
-int getUserInput(){
-    int output;
-    std::string year = "";
-    bool validInput = false;
-    while(!validInput){
-        output = 0;
-        std::cin >> year;
+std::string getUserInput(){
+    std::string input = "";
+    std::cin >> input;
 
-        if(year == "quit"){
-            return -1;
+    for(std::string::iterator it = year.begin(); it != year.end(); ++it){
+        if(*it > 64 || *it < 91){
+            *it += 32;
         }
-
-
-        int count = 1;
-        for(std::string::reverse_iterator it = year.rbegin(); it != year.rend(); ++it){
-            if(*it < 48 || *it > 57){
-                count = -1;
-                break;
-            }
-            output += (*it - 48)*count;
-            count *= 10;
-        }
-
-        if(count == -1){
-            std::cout << "Invalid Input, Please enter a decimal number only using digits from 0-9: ";
-            continue;
-        }
-
-
-        if(output < 2015 || output > 2023){
-            std::cout << "Invalid Input, Please enter a year between and including 2015 and 2023: ";
-            continue;
-        }
-        validInput = true;
+    }
         
-        return output;
-    }
-    return -1;
+    return input;
 }
 
 /*
-@brief: Takes the values and indexes out of the resultSet objects, and puts them into an array
-@param: ResultSet* resSet: the result set to take the values from
-@param: Int[] array: The array to put values into
-@return: void
+@brief: Turns the users input into a command
+@param: std::string userInput, A string containing only the name of the command to be processed
+@param: std::string* commands, a stiring of command names to compare the user input to.
+@param: bool* functions, an array of boolean function pointers to the functions that actually do the hard work
+@param: Connection* connect, a pointer to the connection object
+@return: bool: 1 if the command successfully was processed, 0 otherwise
 */
-void prepareResults(ResultSet* resSet, int* array){
-    while(resSet->next()){
-        //The Month number is in the 2nd column, so subtracting 1 gives the index.
-        //The count from the query is in the first column
-        array[resSet->getInt(2) - 1] = resSet->getInt(1);
+bool processCommand(std::string &userInput, std::string* commands, bool* functions, Connection* connect){
+    int i;
+    for(i = 0; commands[i] != "\0"; ++i){
+        if(userInput == commands[i]){
+            if(functions[i](connect)){
+                return 1;
+            }
+            return 0;
+        }
     }
+    return 0;
 }
 
 /*
-@brief: Takes the values and indexes out of the resultSet objects, and puts them into their respective array
-@param: ResultSet* resSet: the result set to take the values from
-@param: std::string[] names: The array holding the names of the channel/User
-@param: int[] counts: the array holding the message counts for the respective channel/user
+@brief: Takes the string in "string" and executes it via execute update
+@param: Connection* connect: A pointer to the connection object 
+@param: std::string string: the statement to be executed
+@param: the amount of additional user inputs left to get
 @return: void
 */
-void prepareResults(ResultSet* resSet, std::string* names, int* counts){
-    int i = 0;
-    int j = 0;
-    while(resSet->next() && j < 3){
-        names[i++] = resSet->getString(1);
-        names[i++] = resSet->getString(2);
-        counts[j++] = resSet->getInt(3);
+bool update(Connection* connect, std::string string, int count){
+    try{
+        Statement* stmt = connect->createStatement(string);
+        int i = 0;
+        while(i < count){
+            std::string param = "";
+            std::cin >> param;
+            stmt->setString(i++, param);
+        }
+        int result = stmt->executeUpdate();
+        std::cout << "Updated " << result << " Rows!";
+        connect->termniateStatement(stmt);
+    }catch(SQLException &except){
+        std::cerr << "Error " << except.what() << "\n";
+        return 0;
     }
+    return 1;
 }
 
 /*
-@brief: Outputs the data from other functions in a file in the format of a report. (new users and number of messages posted)
-@param: int year: the year the data is from.
-@param: int[] newUsers: The counts of new users that signed up to the platform in the year in year, by month ordered by their index
-@param: int[] postedMessages: The amount of messages posted in the given year, by month ordered by index
-@param: std::ofstream file: the ofstream object coreesponding to the file you want the report in.
+@brief: Takes the string in "string" and executes it via execute update
+@param: Connection* connect: A pointer to the connection object 
+@param: std::string string: the statement to be executed
+@param: int count: The amount of user inputs still yet to be retrieved 
+@param: std::string* columns: An array with the name of all the columns
+@param: int* maxWidth: an array with the maximum amount of characters for each column
+@param: int size: The size of the arrays.
 @return: void
 */
-void generateReport(int year, int* newUsers, int* postedMessages, std::ofstream* file){
-    std::string months[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    *file << "Activity Report By Month for the Year of " << year <<"\n\n"
-         << "       # of New    # of Posted\n"
-         << "Month  Users       Messages\n"
-         << "-----  ---------  -------------\n";
+bool query(Connection* connect, std::string string, int count, std::string* columns, int* maxWidth, int size){
+    try{
+        Statement* stmt = connect->createStatement(string);
+        int i = 0;
+        while(i < count){
+            std::string param = "";
+            std::cin >> param;
+            stmt->setString(i++, param);
+        }
+        ResultSet* rs = stmt->executeQuery();
 
-    for(int i = 0; i < 12; i++){
-        *file << "  " << months[i] << "  " << std::setw(9) << newUsers[i] << "  " << std::setw(13) << postedMessages[i] << "\n";
+        int i = 0;
+        for(; i < size; i++){
+            std::cout << std::setw(maxWidth[i]) << columns[i];
+        }
+
+        i = 0;
+        std::cout << "\n";
+        while(rs->next()){
+            std::cout << std::setw(maxWidth[i]) << rs->getString;
+
+            if(++i >= size){
+                i = 0;
+                std::cout << "\n";
+            }
+        }
     }
-
-    *file << std::setfill('=') << std::setw(50) << "\n";
 }
 
-/*
-@brief: Outputs the data from other functions in a file in the format of a report.
-@param: std::string names[]: An array with the usernames/channelNames (even indexes) and nicknames/poster (odd indexes) of users/channels
-@param: int[] counts: The number of messages posted by/in the given user/channel
-@param: std::ofstream file: the ofstream object coreesponding to the file you want the report outputted to.
-@return: void
-*/
-void generateReport(std::string* names, int* counts, std::ofstream* file){
-    for(int i = 0; i < 3; i++){
-        *file << std::left << std::setfill(' ') << std::setw(15) << names[2*i] << "  " << std::setw(15) << names[2*i + 1] << "  " << std::setw(12) << counts[i] << "\n";
-    }
-}
---
+++ Main.h
@@ -0,0 +1,35 @@
+/*
+Author: Trevor De Clark, Sno: 665182861, CSCI 370 VIU
+*/
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <occi.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string>
+#include <stdexcept>
+using namespace oracle::occi;
+
+#include "functions.h"
+
+#define NUM_OF_COMMANDS 7
+
+// creates a connection to the oracle db by getting the users login info
+Connection* login(Environment* ptr);
+
+// termniates the provided connection and environment
+void logoff(Environment* ptr, Connection* ptr2);
+
+// Gets input from the user using std::cout, and turns all letters to lowercase
+std::string getUserInput();
+
+// Processes the command the user entered, if its valid, it processes it, and returns 1, if not, returns 0
+bool processCommand(std::string &userInput, std::string* commands, bool* functions, connection* connection);
+
+// Creates and executes a sql update statement with the given parameters The amount of parameters is indicated by count.
+bool update(Connection* connect, std::string string, int count);
+
+// Creates and executes a sql query statement with the given parameters The amount of parameters is indicated by count.
+bool query(Connection* connect, std::string string, int count, std::string* columns, int* maxWidth, int size);
